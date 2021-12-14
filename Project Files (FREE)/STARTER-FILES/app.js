e73628fea9268780399d4b2427c262739765170e
Chen Zhao <zhaocsnh@gmail.com>
2021-12-14 14:58:27 -0800

    added logic to display start, stop and reset

--- Project Files (FREE)/STARTER-FILES/app.js
+++ Project Files (FREE)/STARTER-FILES/app.js
@@ -7,9 +7,12 @@ let minutes = document.querySelector('.minutes').children[0]
 let seconds = document.querySelector('.seconds').children[0]
 let totalSeconds = 1 * 3
 
+// console.log(startBtn);
+
 const runPomodoro = () => {
     if (ringColor.classList.contains('ending')) {
         ringColor.classList.remove('ending')
+        // console.log(startBtn.textContent);
         myIntervalFunc()
     } else {
         ringColor.classList.add('ending')
@@ -19,21 +22,31 @@ const runPomodoro = () => {
 
 const myIntervalFunc = () => {
     if (minutes.value == '00' && seconds.value == '00') {
+        if (startBtn.textContent === 'start') {
+            console.log(startBtn.textContent);
+            startBtn.textContent = 'stop' 
+            console.log(startBtn.textContent);
+        } else if (startBtn.textContent === 'stop') {
+            startBtn.textContent = 'start'
+        } else if (startBtn.textContent === 'reset') {
+            startBtn.textContent = 'stop'
+        }
         ringColor.classList.add('ending')
         minutes.value = '00'
         seconds.value = '03'
         totalSeconds = Number(minutes.value * MINUTESTOSECONDS) + Number(seconds.value)
-        setInterval(myClearIntervalFunc, 1000)
+        setInterval(myClearIntervalFunc, 1000) // Why does this work?
     } else {
         setInterval(() => {
             {
                 if (totalSeconds > ONESECOND) {
+                    
                     totalSeconds -= ONESECOND
                     remainingMinutes = Math.floor(totalSeconds / MINUTESTOSECONDS)
                     remainngSeconds = totalSeconds % MINUTESTOSECONDS
                     minutes.value = remainingMinutes
                     seconds.value = remainngSeconds
-                    console.log(totalSeconds, minutes, seconds)
+                    // console.log(totalSeconds, minutes, seconds)
                 } else if (totalSeconds === ONESECOND) {
                     minutes.value = '00'
                     seconds.value = '00'
@@ -45,6 +58,9 @@ const myIntervalFunc = () => {
 }
 
 function myClearIntervalFunc() {
+    if (minutes.value == '00' && seconds.value == '00') {
+        startBtn.textContent = 'reset'
+    }
     clearInterval(myIntervalFunc)
 }
 
