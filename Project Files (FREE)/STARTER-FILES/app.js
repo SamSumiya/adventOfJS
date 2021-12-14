31360e46438c4a26b265f3f980a5ad96d88bf415
Chen Zhao <zhaocsnh@gmail.com>
2021-12-14 13:19:57 -0800

    added feature so that user can restart the timer

--- Project Files (FREE)/STARTER-FILES/app.js
+++ Project Files (FREE)/STARTER-FILES/app.js
@@ -1,31 +1,51 @@
 const ringColor = document.querySelector('.ring')
+const startBtn = document.querySelector('.start')
+const ONESECOND = 1
+const TOTALMINUTES = 15
+const MINUTESTOSECONDS = 60
 let minutes = document.querySelector('.minutes').children[0]
 let seconds = document.querySelector('.seconds').children[0]
-const startBtn = document.querySelector('.start')
+let totalSeconds = 1 * 3
 
-let allSeconds = 1 * 5
-const allMilliseconds = allSeconds * 1000
-
-startBtn.addEventListener('click', () => {
+const runPomodoro = () => {
     if (ringColor.classList.contains('ending')) {
         ringColor.classList.remove('ending')
+        myIntervalFunc()
     } else {
         ringColor.classList.add('ending')
+        myIntervalFunc()
+    }
+}
+
+const myIntervalFunc = () => {
+    if (minutes.value == '00' && seconds.value == '00') {
+        ringColor.classList.add('ending')
+        minutes.value = '00'
+        seconds.value = '03'
+        totalSeconds = Number(minutes.value * MINUTESTOSECONDS) + Number(seconds.value)
+        setInterval(myClearIntervalFunc, 1000)
+    } else {
         setInterval(() => {
             {
-                if (allSeconds > 1) {
-                    allSeconds -= 1
-                    remainingMinutes = Math.floor(allSeconds / 60)
-                    remainngSeconds = allSeconds % 60
-                    // console.log(remainingMinutes, remainngSeconds)
+                if (totalSeconds > ONESECOND) {
+                    totalSeconds -= ONESECOND
+                    remainingMinutes = Math.floor(totalSeconds / MINUTESTOSECONDS)
+                    remainngSeconds = totalSeconds % MINUTESTOSECONDS
                     minutes.value = remainingMinutes
                     seconds.value = remainngSeconds
-                    console.log(allSeconds, minutes, seconds)
-                } else if (allSeconds === 1) {
+                    console.log(totalSeconds, minutes, seconds)
+                } else if (totalSeconds === ONESECOND) {
                     minutes.value = '00'
-                    seconds.value = '00' 
+                    seconds.value = '00'
+                    ringColor.classList.remove('ending')
                 }
             }
         }, 1000)
     }
-})
+}
+
+function myClearIntervalFunc() {
+    clearInterval(myIntervalFunc)
+}
+
+startBtn.addEventListener('click', () => runPomodoro())
