1de5f71c8f7c8ea2338715610702e7e91b64e99a
Chen Zhao <zhaocsnh@gmail.com>
2021-12-14 11:35:47 -0800

    pomodoro stops when time is up

--- Project Files (FREE)/STARTER-FILES/app.js
+++ Project Files (FREE)/STARTER-FILES/app.js
@@ -3,23 +3,29 @@ let minutes = document.querySelector('.minutes').children[0]
 let seconds = document.querySelector('.seconds').children[0]
 const startBtn = document.querySelector('.start')
 
-let allSeconds = 15 * 60
+let allSeconds = 1 * 5
 const allMilliseconds = allSeconds * 1000
 
 startBtn.addEventListener('click', () => {
-    if(ringColor.classList.contains('ending')) {
+    if (ringColor.classList.contains('ending')) {
         ringColor.classList.remove('ending')
-           
     } else {
         ringColor.classList.add('ending')
         setInterval(() => {
-            allSeconds -= 1
-            remainingMinutes = Math.floor(allSeconds / 60) 
-            remainngSeconds = allSeconds % 60
-            console.log(remainingMinutes, remainngSeconds)
-            minutes.value = remainingMinutes
-            seconds.value = remainngSeconds
-            console.log(allSeconds, minutes, seconds)
+            {
+                if (allSeconds > 1) {
+                    allSeconds -= 1
+                    remainingMinutes = Math.floor(allSeconds / 60)
+                    remainngSeconds = allSeconds % 60
+                    // console.log(remainingMinutes, remainngSeconds)
+                    minutes.value = remainingMinutes
+                    seconds.value = remainngSeconds
+                    console.log(allSeconds, minutes, seconds)
+                } else if (allSeconds === 1) {
+                    minutes.value = '00'
+                    seconds.value = '00' 
+                }
+            }
         }, 1000)
     }
 })
