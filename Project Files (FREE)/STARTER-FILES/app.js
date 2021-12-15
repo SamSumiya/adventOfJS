c62a3566f9f5c2580c99b7ff3cae1056b623d0e9
Chen Zhao <zhaocsnh@gmail.com>
2021-12-14 20:04:45 -0800

    fixed an issue that the second timer goes way too fast

--- Project Files (FREE)/STARTER-FILES/app.js
+++ Project Files (FREE)/STARTER-FILES/app.js
@@ -5,65 +5,98 @@ const TOTALMINUTES = 15
 const MINUTESTOSECONDS = 60
 let minutes = document.querySelector('.minutes').children[0]
 let seconds = document.querySelector('.seconds').children[0]
-let totalSeconds = 1 * 3
+let totalSeconds = 1 * 5
 
 const runPomodoro = () => {
     if (ringColor.classList.contains('ending')) {
         ringColor.classList.remove('ending')
-        myClearIntervalFunc()
+        myIntervalFunc()
     } else {
         ringColor.classList.add('ending')
-        myClearIntervalFunc()
+        myIntervalFunc()
     }
 }
 
 const myIntervalFunc = () => {
     if (minutes.value == '00' && seconds.value == '00') {
-        if (startBtn.textContent === 'start') {
-            startBtn.textContent = 'stop'
-        } else if (startBtn.textContent === 'stop') {
-            startBtn.textContent = 'start'
-        } else if (startBtn.textContent === 'reset') {
-            startBtn.textContent = 'stop'
-        }
         ringColor.classList.add('ending')
-        minutes.value = '00'
-        seconds.value = '03'
+        minutes.value = '1'
+        seconds.value = '5'
         totalSeconds = Number(minutes.value * MINUTESTOSECONDS) + Number(seconds.value)
-    } else if (minutes.value == '00' && seconds.value != '00') {
-        minutes.value = '00'
-        seconds.value = totalSeconds - 1
-        totalSeconds = Number(minutes.value * MINUTESTOSECONDS) + Number(seconds.value)
-    } else {
-        if (startBtn.textContent === 'start') {
-            startBtn.textContent = 'stop'
-        } else if (startBtn.textContent === 'stop') {
-            startBtn.textContent = 'start'
-        }
-        setInterval(() => {
-            {
-                if (totalSeconds > ONESECOND) {
-                    totalSeconds -= ONESECOND
-                    remainingMinutes = Math.floor(totalSeconds / MINUTESTOSECONDS)
-                    remainngSeconds = totalSeconds % MINUTESTOSECONDS
-                    minutes.value = remainingMinutes
-                    seconds.value = remainngSeconds
-                } else if (totalSeconds === ONESECOND) {
-                    minutes.value = '00'
-                    seconds.value = '00'
-                    ringColor.classList.remove('ending')
-                    startBtn.textContent = 'reset'
-                }
+        const secondInterval = setInterval(() => {
+            if (totalSeconds > ONESECOND) {
+                totalSeconds -= ONESECOND
+                remainingMinutes = Math.floor(totalSeconds / MINUTESTOSECONDS)
+                remainngSeconds = totalSeconds % MINUTESTOSECONDS
+                minutes.value = remainingMinutes
+                seconds.value = remainngSeconds
+            } else if (totalSeconds === ONESECOND) {
+                minutes.value = '00'
+                seconds.value = '00'
+                ringColor.classList.remove('ending')
+                startBtn.textContent = 'reset'
             }
         }, 1000)
+            if (startBtn.textContent === 'start') {
+                startBtn.textContent = 'pause'
+                myClearIntervalFunc(firstInterval)
+        } else if (startBtn.textContent === 'pause') {
+                startBtn.textContent = 'start'
+                myClearIntervalFunc(secondInterval)
+        } else if (startBtn.textContent === 'reset') {
+                startBtn.textContent = 'pause'
+                myClearIntervalFunc(secondInterval)
+        }
+        // clearInterval(secondInterval)
+    } 
+    // else if (minutes.value == '00' || seconds.value != '00') {
+    //     minutes.value = '00'
+    //     seconds.value = totalSeconds - 1
+    //     totalSeconds = Number(minutes.value * MINUTESTOSECONDS) + Number(seconds.value)
+    //     setInterval(() => { 
+    //         if (totalSeconds > ONESECOND) {
+    //             totalSeconds -= ONESECOND
+    //             remainingMinutes = Math.floor(totalSeconds / MINUTESTOSECONDS)
+    //             remainngSeconds = totalSeconds % MINUTESTOSECONDS
+    //             minutes.value = remainingMinutes
+    //             seconds.value = remainngSeconds
+    //         }   
+    //     }, 1000)
+    // } 
+    else {
+        const firstInterval = setInterval(() => {
+            if (totalSeconds > ONESECOND) {
+                totalSeconds -= ONESECOND
+                remainingMinutes = Math.floor(totalSeconds / MINUTESTOSECONDS)
+                remainngSeconds = totalSeconds % MINUTESTOSECONDS
+                minutes.value = remainingMinutes
+                seconds.value = remainngSeconds
+            } else if (totalSeconds === ONESECOND) {
+                // totalSeconds -= ONESECOND
+                minutes.value = '00'
+                seconds.value = '00'
+                ringColor.classList.remove('ending')
+                startBtn.textContent = 'reset'
+            }
+        }, 1000)
+        if (startBtn.textContent === 'start') {
+            startBtn.textContent = 'pause'
+            // clearInterval(firstInterval)
+        } else if (startBtn.textContent === 'pause') {
+            startBtn.textContent = 'start'
+            myClearIntervalFunc(firstInterval)
+        }
+        // clearInterval(firstInterval)
     }
 }
 
-function myClearIntervalFunc() {
-    if (minutes.value === '00' && seconds.value === '00') {
-        startBtn.textContent = 'reset'
-    }
-    myIntervalFunc()
+function myClearIntervalFunc(x) {
+    // if (minutes.value === '00' && seconds.value === '00') {
+    //     startBtn.textContent = 'reset'
+    // }
+    // if (startBtn.textContent === 'pause') {
+        clearInterval(x)
+    // }
 }
 
 startBtn.addEventListener('click', () => runPomodoro())
